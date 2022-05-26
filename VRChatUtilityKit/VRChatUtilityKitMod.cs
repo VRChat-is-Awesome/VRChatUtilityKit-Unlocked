778369c2b06ad62bacf1aa445e245695bc40a626
ljoonal <ljoonal@ljoonal.xyz>
2022-05-26 16:12:26 +0300

    Bump version

--- VRChatUtilityKit/VRChatUtilityKitMod.cs
+++ VRChatUtilityKit/VRChatUtilityKitMod.cs
@@ -5,7 +5,7 @@ using MelonLoader;
 using VRChatUtilityKit.Ui;
 using VRChatUtilityKit.Utilities;
 
-[assembly: MelonInfo(typeof(VRChatUtilityKit.VRChatUtilityKitMod), "VRChatUtilityKit", "1.4.3", "Sleepers", "https://github.com/SleepyVRC/Mods")]
+[assembly: MelonInfo(typeof(VRChatUtilityKit.VRChatUtilityKitMod), "VRChatUtilityKit", "1.4.4", "Sleepers", "https://github.com/SleepyVRC/Mods")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonPriority(-100)]
 
